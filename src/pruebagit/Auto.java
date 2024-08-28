ee92051b898246eb8b0ac5bf5b93516fadafdcf2
vina <vina@VIAWSE02LC0210.duoc.acad>
2024-08-28 14:34:36 -0400

    probando git

---
+++ src/pruebagit/Auto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author vina
+ */
+public class Auto {
+
+    private String marca, modelo, color tipoTrans
+    }
+    
+}
